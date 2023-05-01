49d819333971629848e8cefd007618800ad11b5e
Your Name Here <your@email.com>
2023-05-01 21:02:41 +0200

    fix docker

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
-docker run -it \
-  --name dev \
-  -u dev \
-  -v ~/Documents/Workspace:/home/dev/workspace \
-  dev
+dockerId="dev"
+
+if [ ! "$(docker ps -a -q -f name=$dockerId)" ]; then
+  docker run -it \
+    --name $dockerId \
+    -u $dockerId \
+    -v ~/Documents/Workspace:/home/dev/workspace \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    $dockerId
+else
+  if [ "$(docker ps -aq -f status=exited -f name=$dockerId)" ]; then
+    docker start dev
+    docker exec -it dev zsh
+  else
+    docker attach dev
+  fi
+fi
